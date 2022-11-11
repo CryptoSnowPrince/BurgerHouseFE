6751a3ed58a20511f016efd74c53e370ea2284af
DESKTOP-V2O0D8E\Hari <topdirector2017@gmail.com>
2022-11-11 09:36:00 +0900

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -366,51 +366,35 @@ const Home = () => {
             <div class="menu-fixed-left">
               <div class="menu-bars">
                 <div class="menu-bar">
+                  <div class="menu-bar-wallet">
+                    <i class="fa fa-wallet" style={{ color: "#e6a71e", marginTop: "-9px" }}></i>
+                  </div>
+                  <div class="menu-bar-value menu-bar-money-value" style={{ left: "55%" }}>{`${curAcount.toString().substr(0, 6)}...${curAcount.toString().substr(38, 41)}`}</div>
+                </div>
+                <div class="menu-bar">
                   <div class="menu-bar-coin"></div>
                   <div class="menu-bar-value menu-bar-money-value">0.00</div>
                   <button type="button" class="menu-bar-btn-plus" onClick={() => setShowBuyCoins(true)} />
                 </div>
-                <div class="menu-bar">
-                  <div class="menu-bar-gas"></div>
-                  <div class="menu-bar-value menu-bar-money-value">0.00</div>
-                  <button type="button" class="menu-bar-btn-minus" onClick={() => setShowGetBNB(true)} />
-                </div>
                 <div class="menu-bar fc-bar">
                   <div class="menu-bar-money"></div>
                   <div class="menu-bar-value menu-bar-money-value">0.00</div>
+                  <button type="button" class="menu-bar-btn-minus" onClick={() => setShowGetBNB(true)} />
                 </div>
-                <div class="menu-bar fc-bar ls-bar">
-                  <div class="menu-bar-market"></div>
-                  <div class="menu-bar-value menu-bar-money-value">0.00</div>
-                </div>
-                <div class="menu-bar">
-                  <div class="menu-bar-peso"></div>
-                  <div class="menu-bar-value menu-bar-money-value">1 CASH = ₱ 0.75</div>
-                </div>
-                <div class="menu-bar">
-                  <div class="menu-bar-exchange"></div>
-                  <div class="menu-bar-value menu-bar-money-value sell-allotment" id="allotment_amount">₱ 2,500,000</div>
-                </div>
+                <div class="menu-bar-value menu-bar-money-value menu-bar-without-background">+0.00/h</div>
               </div>
             </div>
             <div class="menu-fixed-right">
               <div class="menu-btns">
-                <button class="menu-btn-red" id="send_cash" data-bs-placement="right" data-bs-toggle="tooltip" title="Send FC/LC">
-                  <i class="fa fa-money-bill-wave"></i>
-                </button>
-                <button class="menu-btn menu-btn-leaderboard" data-bs-placement="right" data-bs-toggle="tooltip" title="Leaderboard">
-                  <i class="fa fa-trophy"></i>
-                </button>
-                <button class="menu-btn menu-btn-affiliate" data-bs-placement="right" data-bs-toggle="tooltip" title="Affiliate"  >
+                <button class="menu-btn menu-btn-affiliate" data-bs-placement="right" data-bs-toggle="tooltip" title="Partners"  >
                   <i class="fa fa-users"></i>
                 </button>
-                <button class="menu-btn menu-btn-transactions" data-bs-placement="right" data-bs-toggle="tooltip" title="Transactions">
-                  <i class="fa fa-cubes"></i>
+                <button class="menu-btn menu-btn-leaderboard" data-bs-placement="right" data-bs-toggle="tooltip" title="Telegram">
+                  <svg fill="#ffffff" xmlns="http://www.w3.org/2000/svg" viewBox="3 5 50 50" width="30px" height="30px"><path d="M46.137,6.552c-0.75-0.636-1.928-0.727-3.146-0.238l-0.002,0C41.708,6.828,6.728,21.832,5.304,22.445	c-0.259,0.09-2.521,0.934-2.288,2.814c0.208,1.695,2.026,2.397,2.248,2.478l8.893,3.045c0.59,1.964,2.765,9.21,3.246,10.758	c0.3,0.965,0.789,2.233,1.646,2.494c0.752,0.29,1.5,0.025,1.984-0.355l5.437-5.043l8.777,6.845l0.209,0.125	c0.596,0.264,1.167,0.396,1.712,0.396c0.421,0,0.825-0.079,1.211-0.237c1.315-0.54,1.841-1.793,1.896-1.935l6.556-34.077	C47.231,7.933,46.675,7.007,46.137,6.552z M22,32l-3,8l-3-10l23-17L22,32z"></path></svg>
                 </button>
-                <button class="menu-btn menu-btn-logs" data-bs-placement="right" data-bs-toggle="tooltip" title="Logs">
-                  <i class="fa fa-money-check"></i>
+                <button class="menu-btn menu-btn-transactions" data-bs-placement="right" data-bs-toggle="tooltip" title="Help">
+                  <i class="fa fa-question"></i>
                 </button>
-
                 <button class="menu-btn menu-btn-logout" data-bs-placement="right" data-bs-toggle="tooltip" title="Logout" onClick={logoutOfWeb3Modal} >
                   <i class="fa fa-sign-out"></i>
                 </button>
