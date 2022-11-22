d3011b686090407f55871fee8556d06583cced8c
DESKTOP-V2O0D8E\Hari <topdirector2017@gmail.com>
2022-11-22 22:20:44 +0900

    add abis

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,4 +1,5 @@
 import BurgerHouse_ABI from './abi.json'
+import BUSD_ABI from './busd.json'
 
 export const PUBLIC_URL = "https://dapp-frontend-prince.web.app"
 
@@ -23,7 +24,7 @@ export const ADMIN_ACCOUNT = '0x36285fDa2bE8a96fEb1d763CA77531D696Ae3B0b'
 export const ADMIN_ACCOUNT1 = '0x2faf8ab2b9ac8Bd4176A0B9D31502bA3a59B4b41'
 
 export const BurgerHouse = '0x8F104A87ebF613506cEFdA2143CF34603C907313'
-export const BUSD_ADDRESS = "0x7A62eE9B6cde5cdd3Fd9d82448952f8E2f99c8C0";
+export const BUSD = "0x7A62eE9B6cde5cdd3Fd9d82448952f8E2f99c8C0";
 
 export const RPC_URL = "https://bsc-testnet.public.blastapi.io"
 export const MAINNET = 97
@@ -33,7 +34,7 @@ export const MAINNET = 97
 // export const ADMIN_ACCOUNT1 = '0xAD30Ed907f394cF1E426c2F3Fa2Ba18B46aC6E66'
 
 // export const BurgerHouse = '0x370D30b3f8437c2f40E203c1307e84a4BB6d672e'
-// export const BUSD_ADDRESS = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
+// export const BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
 
 // export const RPC_URL = "https://bsc-dataseed1.binance.org"
 // export const MAINNET = 56
@@ -42,4 +43,8 @@ export const REF_PREFIX = `${PUBLIC_URL}/?ref=`
 
 export function getBurgerHouseContract(web3) {
 	return new web3.eth.Contract(BurgerHouse_ABI, BurgerHouse);
+}
+
+export function getBUSDContract(web3) {
+	return new web3.eth.Contract(BUSD_ABI, BUSD);
 }
