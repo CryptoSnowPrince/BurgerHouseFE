3343393c2ab6da7e8c0e041183ce07bbfb41aa66
DESKTOP-V2O0D8E\Hari <topdirector2017@gmail.com>
2022-11-30 20:20:54 +0900

    switch on Mainnet

--- src/constant/index.js
+++ src/constant/index.js
@@ -19,24 +19,26 @@ export const ALERT_POSITION = 'top-center';
 export const LAUNCH_TIME = 1670166000;
 
 // BSC TESTNET
-export const ADMIN_ACCOUNT = '0x36285fDa2bE8a96fEb1d763CA77531D696Ae3B0b'
-export const ADMIN_ACCOUNT1 = '0x2faf8ab2b9ac8Bd4176A0B9D31502bA3a59B4b41'
+// export const ADMIN_ACCOUNT = '0x36285fDa2bE8a96fEb1d763CA77531D696Ae3B0b'
+// export const ADMIN_ACCOUNT1 = '0x2faf8ab2b9ac8Bd4176A0B9D31502bA3a59B4b41'
 
-export const BurgerHouse = '0x40CFa8f3D97D3e3eC59BD0d966E98612DEe6cA3b'
-export const BUSD = "0x7A62eE9B6cde5cdd3Fd9d82448952f8E2f99c8C0";
+// // export const BurgerHouse = '0x40CFa8f3D97D3e3eC59BD0d966E98612DEe6cA3b'
+// export const BurgerHouse = '0x547E3cc6DA1aCdc476CD4a0F03B798071f86a320'
+// export const BUSD = "0x7A62eE9B6cde5cdd3Fd9d82448952f8E2f99c8C0";
 
-export const RPC_URL = "https://bsc-testnet.public.blastapi.io"
-export const MAINNET = 97
+// export const RPC_URL = "https://bsc-testnet.public.blastapi.io"
+// export const MAINNET = 97
 
 // BSC MAINNET
-// export const ADMIN_ACCOUNT = '0xc50F0919AB4c2b779387Eb04ab984fee37D70b38'
-// export const ADMIN_ACCOUNT1 = '0xAD30Ed907f394cF1E426c2F3Fa2Ba18B46aC6E66'
+export const ADMIN_ACCOUNT = '0x2Cc4467e7a94D55497B704a0acd90ACd1BF9A5af'
+export const ADMIN_ACCOUNT1 = '0xAD30Ed907f394cF1E426c2F3Fa2Ba18B46aC6E66'
 
-// export const BurgerHouse = '0x370D30b3f8437c2f40E203c1307e84a4BB6d672e'
+export const BurgerHouse = '0xa58129841d3Eb3B575f5773A2C04ac1fc8AA6d3c'
+export const BUSD = "0xb7b657071Ad838AEB0096597f071AF981cdD4c9a"; // MockBUSD
 // export const BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
 
-// export const RPC_URL = "https://bsc-dataseed1.binance.org"
-// export const MAINNET = 56
+export const RPC_URL = "https://bsc-dataseed1.binance.org"
+export const MAINNET = 56
 
 export const REF_PREFIX = `${PUBLIC_URL}/?ref=`
 
