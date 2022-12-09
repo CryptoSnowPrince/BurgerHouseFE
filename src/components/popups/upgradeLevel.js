84e7316a0512032f6aa121ac800e03fc1f268b1e
DESKTOP-V2O0D8E\Hari <topdirector2017@gmail.com>
2022-12-10 04:42:51 +0900

    complete

--- src/components/popups/upgradeLevel.js
+++ src/components/popups/upgradeLevel.js
@@ -85,14 +85,14 @@ const UpgradeLevel = ({
                                     timer !== "" ?
                                         <>
                                             <div className="popup-time-icon" />
-                                            <div className="level-text" style={{ marginLeft: "1px" }}>
+                                            <div className="level-text-top-level" style={{ marginLeft: "1px" }}>
                                                 {timer}
                                             </div>
                                         </>
                                         :
                                         <>
                                             <div className="farm-coin" >&nbsp;</div>
-                                            <div className="level-text">
+                                            <div className="level-text-top-level">
                                                 {price[level + 5 * (houseId - 1)]}
                                             </div>
                                         </>
